acd07d3ce82218e1c2e9426d6b8daaa49a19a102
priyal <priyaljaiin52@gmail.com>
2024-03-20 17:02:24 +0530

    stars issue fixed

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
   const [thumbsUpClicked, setThumbsUpClicked] = useState(false);
   const [thumbsDownClicked, setThumbsDownClicked] = useState(false);
   const [selectedButton, setSelectedButton] = useState(null);
+  const [safetyRating, setSafetyRating] = useState(0);
+  const [communicationRating, setCommunicationRating] = useState(0);
   const data = [
     { index: 0, title: "Adventurous" },
     { index: 1, title: "Good Listener" },
@@ -31,24 +33,24 @@ export default function Home() {
   return (
     <div className="m-2 p-4 flex flex-col sm:items-center sm:justify-center  ">
       <div className="flex flex-col gap-4 mt-6">
-        <header className="text-3xl font-extrabold">Leave a Review</header>
+        <header className="text-3xl font-extrabold 2xl:text-5xl">Leave a Review</header>
         <div>
-          <h2 className="mt-2 font-semibold">Safety</h2>
-          <h7 className="text-slate-400 text-[10px] ">
+          <h2 className="mt-2 font-semibold 2xl:text-3xl 2xl:mt-5">Safety</h2>
+          <h7 className="text-slate-400 text-[10px] 2xl:text-[15px] ">
             How safe did you feel with Trausti?
           </h7>
-          <ReactStars count={5} size={40} color2={"#ffd700"} />
+          <ReactStars count={5} size={40} color2={"#ffd700"} onChange={setSafetyRating} value={safetyRating} />
         </div>
 
         <div>
-          <h2 className="mt-2 font-semibold">Communication</h2>
-          <h7 className="text-slate-400 text-[10px] leading-[0.5]">
+          <h2 className="mt-2 font-semibold 2xl:text-3xl">Communication</h2>
+          <h7 className="text-slate-400 text-[10px] leading-[0.5] 2xl:text-[15px] ">
             Lorem ipsum, dolor sit amet consectetur ?
           </h7>
-          <ReactStars count={5} size={40} color2={"#ffd700"} />
+          <ReactStars count={5} size={40} color2={"#ffd700"} onChange={setCommunicationRating} value={communicationRating} />
         </div>
         <div>
-          <h2 className="font-semibold normal-case">
+          <h2 className="font-semibold normal-case 2xl:text-3xl">
             Would you recommend Trausti?
           </h2>
           <Button onClick={handleThumbsDownClick} disabled={thumbsDownClicked}>
@@ -61,8 +63,8 @@ export default function Home() {
           </Button>
         </div>
         <div>
-          <h2 className="font-semibold">Praise</h2>
-          <h7 className="text-slate-400 text-[10px] leading-[0.5]">
+          <h2 className="font-semibold 2xl:text-3xl">Praise</h2>
+          <h7 className="text-slate-400 text-[10px] leading-[0.5] 2xl:text-[15px]">
             Lorem ipsum, dolor sit amet consectetur ?
           </h7>
           <div className="flex flex-wrap gap-3 mt-4">
@@ -72,7 +74,7 @@ export default function Home() {
                   key={val.index}
                   className={`border-1 rounded-full text-black ${
                     selectedButton === val.index
-                      ? "bg-green-300"
+                      ? "bg-green-500"
                       : "bg-gray-200"
                   }`}
                   onClick={() => handleButtonClick(val.index)}
